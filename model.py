2ad804e3fe6a5568abb439163a5e7872440c13af
Кристина <kristi090217@gmail.com>
2023-12-12 21:45:03 +0300

    fix

--- model.py
+++ model.py
@@ -1,84 +1,41 @@
-# # imports
-# from keras import layers
-# from keras import models
-# from keras import optimizers
-# from keras.preprocessing.image import ImageDataGenerator
-# import matplotlib.pyplot as plt
-# # layers, activation layer with 57 nodes (one for every symbol)
-# model = models.Sequential()
-# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(256, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(256, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-# model.add(layers.Flatten())
-# model.add(layers.Dropout(0.5))
-# model.add(layers.Dense(512, activation='relu'))
-# model.add(layers.Dense(57, activation='softmax'))
-# model.compile(loss='categorical_crossentropy',       optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
-# # specify the directories
-#
-# train_dir = 'symbols/train'
-# validation_dir = 'symbols/validation'
-# test_dir = 'symbols/test'
-# # data augmentation with ImageDataGenerator from Keras (only train)
-# train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.1, zoom_range=0.1, horizontal_flip=True, vertical_flip=True)
-# test_datagen = ImageDataGenerator(rescale=1./255)
-# train_generator = train_datagen.flow_from_directory(train_dir, target_size=(400,400), batch_size=20, class_mode='categorical')
-# validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(400,400), batch_size=20, class_mode='categorical')
-#
-# history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100, validation_data=validation_generator, validation_steps=50)
-# # don't forget to save your model!
-# model.save('models/model.h5')
-
-import numpy as np
-from keras.models import load_model
-from keras.preprocessing import image
+# imports
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+# layers, activation layer with 57 nodes (one for every symbol)
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(400, 400, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(57, activation='softmax'))
+model.compile(loss='categorical_crossentropy',       optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+# specify the directories
 
-# Load the saved model
-model = load_model('models/model.h5')
+train_dir = 'symbols/train'
+validation_dir = 'symbols/validation'
+test_dir = 'symbols/test'
+# data augmentation with ImageDataGenerator from Keras (only train)
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.1, zoom_range=0.1, horizontal_flip=True, vertical_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(400,400), batch_size=20, class_mode='categorical')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(400,400), batch_size=20, class_mode='categorical')
 
-# Load the image you want to make predictions on
-image_path = 'dataset_cards/1.jpg'  # Replace with the path to your image
-img = image.load_img(image_path, target_size=(400, 400))
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-img_array /= 255.0  # Rescale to match the training data preprocessing
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100, validation_data=validation_generator, validation_steps=50)
+# don't forget to save your model!
+model.save('models/model.h5')
 
-# Make predictions
-predictions = model.predict(img_array)
-
-# Assuming you have a dictionary that maps class indices to class names
-class_indices = {0: 'class_0', 1: 'class_1', 2: 'class_2'}  # Update with your actual mapping
-
-# Get information about predictions
-predicted_labels = np.argmax(predictions, axis=1)
-predicted_probabilities = np.max(predictions, axis=1)
-
-# Display the image along with bounding boxes and class names
-plt.imshow(img)
-
-ax = plt.gca()
-
-for label, probability in zip(predicted_labels, predicted_probabilities):
-    class_name = class_indices.get(label, 'Unknown')
-
-    # Draw a bounding box
-    rect = patches.Rectangle((0, 0), 400, 400, linewidth=2, edgecolor='r', facecolor='none')
-    ax.add_patch(rect)
-
-    # Annotate with class name and probability
-    plt.text(10, 10, f'{class_name}: {probability:.2f}', color='r', backgroundcolor='w')
-
-plt.axis('off')
-plt.show()
 
 
--- prob.py
+++ prob.py
@@ -4,45 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+
 def process_and_save_images(input_folder, output_folder):
+    # Проверяем, существует ли папка вывода, и создаем ее, если нет
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
+    # Проходим по каждому файлу в папке ввода
     for filename in os.listdir(input_folder):
+        # Проверяем, имеет ли файл допустимое расширение изображения
         if filename.endswith(('.jpg', '.jpeg', '.png')):
-            imgname = os.path.splitext(filename)[0]
+            imgname = os.path.splitext(filename)[0]  # Получаем имя изображения без расширения
+
+            # Загружаем изображение
             image = cv2.imread(os.path.join(input_folder, filename))
-            lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-            l, a, b = cv2.split(lab)
 
-            clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
-            cl = clahe.apply(l)
-            limg = cv2.merge((cl, a, b))
-            final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-
-            resized = cv2.resize(final, (800, 800))
-            # cv2.imwrite(os.path.join(output_folder, f'{imgname}_processed.jpg'), resized)
-
-            image = resized
             gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-            thresh = cv2.threshold(gray, 190, 255, cv2.THRESH_BINARY)[1]
-            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cnts = imutils.grab_contours(cnts)
-            output = image.copy()
-
-            for c in cnts:
-                cv2.drawContours(output, [c], -1, (255, 0, 0), 3)
-
-            cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
-            mask = np.zeros(gray.shape, np.uint8)
-            mask = cv2.drawContours(mask, [cnts], -1, 255, cv2.FILLED)
-            fg_masked = cv2.bitwise_and(image, image, mask=mask)
-            mask = cv2.bitwise_not(mask)
-            bk = np.full(image.shape, 255, dtype=np.uint8)
-            bk_masked = cv2.bitwise_and(bk, bk, mask=mask)
-            final = cv2.bitwise_or(fg_masked, bk_masked)
-
-            gray = cv2.cvtColor(final, cv2.COLOR_RGB2GRAY)
             thresh = cv2.threshold(gray, 195, 255, cv2.THRESH_BINARY)[1]
             thresh = cv2.bitwise_not(thresh)
             cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -75,21 +52,15 @@ def process_and_save_images(input_folder, output_folder):
                     if not roi.size == 0 and roi.shape[0] > 0 and roi.shape[1] > 0:
                         roi = cv2.resize(roi, (400, 400))
 
+                        # Создаем папку для символов, если ее не существует
                         icon_folder = os.path.join(output_folder, 'symbols', imgname)
                         if not os.path.exists(icon_folder):
                             os.makedirs(icon_folder)
 
+                        # Сохраняем обработанный символ
                         cv2.imwrite(os.path.join(icon_folder, f"simb_{i}.jpg"), roi)
-                        plt.imshow(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
-                        plt.title('Contours')
-                        plt.show()
-                        i += 1
-
-                    plt.imshow(cv2.cvtColor(final, cv2.COLOR_BGR2RGB))
-                    plt.title('Contours')
-                    plt.show()
 
 # Пример использования
-input_folder = "f"
+input_folder = "augmented_dataset"
 output_folder = "output"
 process_and_save_images(input_folder, output_folder)
